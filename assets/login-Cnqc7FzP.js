3ac06a5a250a3c8b5e9a4b6cc7f8309110f004fe
Praveen Gorumuchu <45392749+praveen-gorumuchu@users.noreply.github.com>
2025-10-22 22:08:30 +0530

    Updates

--- assets/login-Cnqc7FzP.js
+++
@@ -1,1 +0,0 @@
-import{j as i}from"./index-zFFphpGY.js";function o(){return i.jsx("div",{children:"login"})}export{o as default};
--- assets/wishList-NmlSz9wx.js
+++
@@ -1,1 +0,0 @@
-import{j as i}from"./index-zFFphpGY.js";function t(){return i.jsx("div",{children:"wishList"})}export{t as default};
