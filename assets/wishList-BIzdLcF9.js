ba61a864b044d63a090c0f96600caab9973e3086
Praveen Gorumuchu <45392749+praveen-gorumuchu@users.noreply.github.com>
2025-10-22 22:04:23 +0530

    Updates

---
+++ assets/wishList-BIzdLcF9.js
@@ -0,0 +1,1 @@
+import{j as i}from"./index-CCqRiAXC.js";function t(){return i.jsx("div",{children:"wishList"})}export{t as default};
--- assets/wishList-CqFR9e75.js
+++
@@ -1,1 +0,0 @@
-import{j as i}from"./index-CsCeYaqx.js";function t(){return i.jsx("div",{children:"wishList"})}export{t as default};
